00126d6db7504f30812b4764159f8e6ea380df7f
QuyetTC <tranquyet02588x1112002@gmail.com>
2023-10-24 21:11:14 +0700

    Solve Confict

--- app/src/main/java/com/example/healthcarecomp/common/Constant.kt
+++ app/src/main/java/com/example/healthcarecomp/common/Constant.kt
@@ -6,6 +6,10 @@ class Constant {
     companion object{
         const val BASE_URL: String = "https://mockapi.io/projects/648fd2c81e6aa71680ca1f63"
         const val APP_DATABASE_NAME = "app_db"
+        const val FIREBASE_DATABASE_URL = "https://healtcarecomp-default-rtdb.asia-southeast1.firebasedatabase.app/"
+        const val DOCTOR_TBL = "doctors"
+        const val PATIENT_TBL = "patients"
+
 
         fun getItemListForRecycleView():  ArrayList<Item_recycleView>  {
             val itemList = ArrayList<Item_recycleView>()
