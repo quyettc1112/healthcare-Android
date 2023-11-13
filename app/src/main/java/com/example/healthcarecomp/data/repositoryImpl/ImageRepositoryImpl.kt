c74410f49d7133654816d5173a7b8382f9f7abc1
PhiNSE <nguyenphigv23@gmail.com>
2023-11-13 16:14:04 +0700

    upload image

--- app/src/main/java/com/example/healthcarecomp/data/repositoryImpl/ImageRepositoryImpl.kt
+++ app/src/main/java/com/example/healthcarecomp/data/repositoryImpl/ImageRepositoryImpl.kt
@@ -4,13 +4,8 @@ import android.content.ContentResolver
 import android.content.Context
 import android.net.Uri
 import android.webkit.MimeTypeMap
-import com.example.healthcarecomp.data.model.User
 import com.example.healthcarecomp.data.repository.ImageRepository
 import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.UploadTask
-import dagger.Component
-import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
 
@@ -25,22 +20,29 @@ class ImageRepositoryImpl @Inject constructor(
         onFailure: (errorMessage: String) -> Unit,
         onProgress: () -> Unit
     ) {
-        val storageRef = firebaseStorage.reference.child("images/${System.currentTimeMillis()}.${getFileExtension(imageUri)}")
-        val uploadTask: UploadTask = storageRef.putFile(imageUri)
-        uploadTask.addOnProgressListener {
-            onProgress()
-        }.addOnCompleteListener{
-            if(it.isSuccessful){
-                onSuccess(Uri.parse(it.result.toString()))
-            } else {
-                onFailure("Upload image failed")
-            }
-        }.addOnCanceledListener{
-            onFailure("Upload image canceled")
+        val storageRef = firebaseStorage.reference.child(
+            "images/${System.currentTimeMillis()}.${
+                getFileExtension(imageUri)
+            }"
+        )
+        val uploadTask = storageRef.putFile(imageUri)
+        uploadTask.addOnProgressListener { taskSnapshot ->
+            val progress =
+                (100.0 * taskSnapshot.bytesTransferred / taskSnapshot.totalByteCount).toInt()
+            onProgress.invoke()
         }
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    storageRef.downloadUrl.addOnSuccessListener { uri ->
+                        onSuccess.invoke(uri)
+                    }
+                } else {
+                    onFailure.invoke("Image upload failed: ${task.exception?.message}")
+                }
+            }
     }
 
-    private fun getFileExtension(uri: Uri): String?{
+    private fun getFileExtension(uri: Uri): String? {
         val contentResolver: ContentResolver = applicationContext.contentResolver
         val mimeTypeMap: MimeTypeMap = MimeTypeMap.getSingleton()
         return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri))
